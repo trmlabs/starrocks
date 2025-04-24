72e944d81cc26ceabc1cf10eb2f6445bb7df877a
malfattiftrm <felipe.malfatti@trmlabs.com>
2025-04-24 11:21:44 -0300

    Update ConnectContext.java
    Signed-off-by: malfattiftrm <felipe.malfatti@trmlabs.com>

--- fe/fe-core/src/main/java/com/starrocks/qe/ConnectContext.java
+++ fe/fe-core/src/main/java/com/starrocks/qe/ConnectContext.java
@@ -83,8 +83,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 // When one client connect in, we create a connection context for it.
 // We store session information here. Meanwhile, ConnectScheduler all
